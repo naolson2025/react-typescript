103c80c02b1e7a46b3d0d80386c0c77ff5e3caf7
Nick Olson <naolson.1819@gmail.com>
2023-04-04 07:33:27 -0500

    custom polugin for unpkg

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,13 +1,47 @@
 import ReactDOM from "react-dom";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
+// import esbuild web assembly. This is a special version of esbuild that runs in the browser
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
+  // using ref instead of state because we don't want to re-render the esbuild component when the value changes
+  const ref = useRef<any>();
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
-  const onClick = () => {
-    console.log(input);
+  useEffect(() => {
+    startService();
+  }, []);
+
+  const onClick = async () => {
+    if (!ref.current) {
+      return;
+    }
+
+    // take the user input code and transpile it from jsx to es2015
+    // const result = await ref.current.transform(input, {
+    //   loader: "jsx",
+    //   target: "es2015",
+    // })
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
+    });
+    setCode(result.outputFiles[0].text);
   }
+
+  // esbuild runs in the browser and can transpile & bundle code
+  const startService = async () => {
+    // example of using a ref on a function component
+    ref.current = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+  };
+
 
   return <div>
     <textarea
---
+++ jbook/src/plugins/unpkg-path-plugin.ts
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import * as esbuild from 'esbuild-wasm';
+
+// create a custom plugin that will override esbuilds default behavior
+export const unpkgPathPlugin = () => {
+  return {
+    name: 'unpkg-path-plugin',
+    setup(build: esbuild.PluginBuild) {
+      // find where a file is stored
+      // override esbuilds default behavior of finding a file
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
+        console.log('onResolve', args);
+        if (args.path === 'index.js') {
+          return { path: args.path, namespace: 'a' };
+        }
+
+        if (args.path.includes('./') || args.path.includes('../')) {
+          return {
+            namespace: 'a',
+            path: new URL(args.path, args.importer + '/').href,
+          };
+        }
+
+        return {
+          namespace: 'a',
+          path: `https://unpkg.com/${args.path}`,
+        }
+        
+        // else if (args.path === 'tiny-test-pkg') {
+        //   return {
+        //     namespace: 'a',
+        //     path: `https://unpkg.com/tiny-test-pkg@1.0.0/index.js`,
+        //     // path: `https://unpkg.com/${args.path}`,
+        //   };
+        // }
+      });
+ 
+      // override esbuilds default behavior of loading a file
+      // from the file system. Instead, we will load it from
+      // unpkg.com
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        console.log('onLoad', args);
+ 
+        if (args.path === 'index.js') {
+          return {
+            loader: 'jsx',
+            contents: `
+              import message from 'medium-test-pkg';
+              console.log(message);
+            `,
+          };
+        }
+
+        const { data } = await axios.get(args.path);
+        return {
+          loader: 'jsx',
+          contents: data,
+        };
+      });
+    },
+  };
+};
