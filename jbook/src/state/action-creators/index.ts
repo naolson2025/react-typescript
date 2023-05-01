ea33bf7569d4c609d6420eef237a644122406de6
Nick Olson <naolson.1819@gmail.com>
2023-05-01 07:45:50 -0500

    made action creators

---
+++ jbook/src/state/action-creators/index.ts
@@ -0,0 +1,52 @@
+import { ActionType } from '../action-types';
+import {
+  DeleteCellAction,
+  Direction,
+  InsertCellBeforeAction,
+  MoveCellAction,
+  UpdateCellAction,
+} from '../actions';
+import { CellTypes } from '../cell';
+
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content,
+    },
+  };
+};
+
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id,
+  };
+};
+
+export const moveCell = (
+  id: string,
+  direction: Direction
+): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction,
+    },
+  };
+};
+
+export const insertCellBefore = (
+  id: string | null,
+  type: CellTypes
+): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type,
+    },
+  };
+};
